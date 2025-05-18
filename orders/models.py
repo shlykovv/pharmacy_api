04186f2684ff53a05d927e1935d1185bace5b46d
Vadim Shlykov <shlikov.vadim@gmail.com>
2025-05-19 00:59:50 +0300

    add model Order and OrderItem for app orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from drugs.models import Drug
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Order #{self.id} by {self.user.username}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, ob_delete=models.CASCADE, related_name='items')
+    drug = models.ForeignKey(Drug, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f'{self.quantity} x {self.drug.name}'
